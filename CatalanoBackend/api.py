fd379c43dba7d31985f64ad2b2ed8c8d3927ab24
silciampe <silciampe@gmail.com>
2024-11-19 16:45:17 -0300

    Anda Agropartes

--- CatalanoBackend/api.py
+++ CatalanoBackend/api.py
@@ -43,5 +43,23 @@ class AgroParteViewSet(viewsets.ModelViewSet):
     queryset = AgroParte.objects.all().order_by('id_catalano')
     serializer_class = AgroParteSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['grupo', 'id_catalano', 'medida_cub', "espesor_mm", 'dientes', 'rad_mm', 'modelo', 'marca', 'cadena', 'observacion', 'diametro_exterior', 'diametro_interior', 'diametro_rodillo', 'cantidad_agujero_x_diametro_agujero', 'cantidad_estrias_x_tipo_rosca', 'cantidad_estrias_x_espesor_estrias', 'imagen']
+    filterset_fields = ['grupo', 'id_catalano', 'medida_cub', "espesor_mm", 'dientes', 'rad_mm', 'modelo', 'marca', 'cadena', 'observacion', 'diametro_exterior', 'diametro_interior', 'diametro_rodillo', 'cantidad_agujero_x_diametro_agujero', 'cantidad_estrias_x_tipo_rosca', 'cantidad_estrias_x_espesor_estrias']
     
+        # Método para obtener marcas según el grupo
+    @action(detail=False, methods=['get'], url_path='marcas')
+    def marcas(self, request):
+        grupo = request.query_params.get('grupo', None)
+        if grupo:
+            marcas = AgroParte.objects.filter(grupo=grupo).values_list('marca', flat=True).distinct()
+            return Response(list(marcas))
+        return Response([])
+
+    # Método para obtener modelos según el grupo y la marca
+    @action(detail=False, methods=['get'], url_path='modelos')
+    def modelos(self, request):
+        grupo = request.query_params.get('grupo', None)
+        marca = request.query_params.get('marca', None)
+        if grupo and marca:
+            modelos = AgroParte.objects.filter(grupo=grupo, marca=marca).values_list('modelo', flat=True).distinct()
+            return Response(list(modelos))
+        return Response([])
